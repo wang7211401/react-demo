f96357cc30af33227b5287120d81e87fdda91587
wang7211401 <wang7211401@163.com>
2018-01-26 20:06:47 +0800

    更新样式

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 import './index.css';
+// import bg from './img/bg.jpg';
+
+// const divStyle = {
+//   backgroundImage: 'url(' + bg + ')'
+// };
 
 ReactDOM.render(
   <App />,
