bfe2e3a10596c6bb1cc0f76b6ef8865dbd1f7818
wang7211401 <wang7211401@163.com>
2018-01-26 22:45:25 +0800

    登录、注册功能

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -9,3 +9,50 @@ AV.init({
 });
 
 export default AV
+
+export function signUp(username,password,sucessFn,errorFn){
+    console.log(username)
+    console.log(password)
+    var user = new AV.User();
+    // 设置用户名
+    user.setUsername(username);
+    // 设置密码
+    user.setPassword(password);
+    // 设置邮箱
+    user.signUp().then(function (loginedUser) {
+        let user = getUserFromAVUser(loginedUser)
+        sucessFn.call(null,user)
+    }, function (error) {
+        errorFn.call(null,error)
+    })
+    return undefined
+}
+
+export function signIn(username,password,sucessFn,errorFn){
+    AV.User.logIn(username, password).then(function (loginedUser) {
+        let user = getUserFromAVUser(loginedUser)
+        sucessFn.call(null,user)
+    }, function (error) {
+        errorFn.call(null,error)
+    });
+    return undefined
+}
+export function getCurrentUser(){
+    let user = AV.User.current()
+    if(user){
+        return getUserFromAVUser(user)
+    }else{
+        return null
+    }
+}
+export function signOut(){
+    AV.User.logOut()
+    return undefined
+}
+function getUserFromAVUser(AVUser){
+    console.log(AVUser)
+    return {
+        id:AVUser.id,
+        ...AVUser.attributes
+    }
+}
