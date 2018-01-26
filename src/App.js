93aa4557f39adbd3da63ff008e9a1e2c107031d8
wang7211401 <wang7211401@163.com>
2018-01-26 18:10:02 +0800

    添加 normalize.css

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import 'normalize.css/normalize.css'
+import './reset.css'
+import './App.css';
+import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      newTodo: '',
+      todoList: [
+      ]
+    }
+  }
+  render() {
+
+    let todos = this.state.todoList.map((item,index)=>{
+      return (
+        <li key={index} >
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)} />
+        </li>
+      )
+    })
+
+    return (
+      <div className="App">
+        <h1>我的待办</h1>
+        <div className="inputWrapper">
+          <TodoInput content={this.state.newTodo} 
+            onChange={this.changeTitle.bind(this)}
+            onSubmit={this.addTodo.bind(this)} />
+        </div>
+        <ol>
+          {todos}
+        </ol>
+      </div>
+    )
+  }
+  toggle(e, todo){
+    todo.status = todo.status === 'completed' ? '' : 'completed'
+    this.setState(this.state) 
+  }
+  changeTitle(event){
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
+  }
+  addTodo(event){
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
+  }
+}
+
+export default App;
+
+let id = 0
+
+function idMaker(){
+  id += 1
+  return id
+}
